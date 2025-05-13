e8e803f5adff3c309d2e94269c1c464512afb15f
Ahmad Baihaqi <143946991+ahmadbyhq@users.noreply.github.com>
2025-05-13 10:00:57 +0700

    database awal 1.2

--- db/db_mtq.sql
+++ db/db_mtq.sql
@@ -46,7 +46,9 @@ CREATE TABLE nilai (
 INSERT INTO users (username, password, nama, role) VALUES
 ('panitia1', 'belanegara', 'Panitia', 'panitia'),
 ('juri1', 'mtqupnyjt', 'Juri1', 'juri'),
-('juri2', 'mhqupnyjt', 'Juri2', 'juri');
+('juri2', 'mhqupnyjt', 'Juri2', 'juri'),
+('juri2', 'mtqupnyjt', 'Juri3', 'juri'),
+('juri2', 'mhqupnyjt', 'Juri4', 'juri');
 
 
 INSERT INTO lomba (nama_lomba) VALUES
