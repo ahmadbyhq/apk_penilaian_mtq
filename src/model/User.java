de9336842f7b3356b695be61b0e0343b93f30c4f
chxbim <christianbimo08@gmail.com>
2025-05-28 15:43:28 +0700

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,59 +1,63 @@
 package model;
 
-public class User {
-    private int id;
-    private String username;
-    private String password;
-    private String nama;
-    private String role;
+public abstract class User {
+    protected int id;
+    protected String username;
+    protected String password;
+    protected String nama;
+    protected String role;
 
-    public User(int id, String username, String password, String nama, String role) {
+    public User(int id, String username, String password, String nama, String role){
         this.id = id;
         this.username = username;
         this.password = password;
         this.nama = nama;
         this.role = role;
-    }
-
-    // Getter
-    public int getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public String getRole() {
-        return role;
     }
 
     // Setter
-    public void setId(int id) {
+    public int getId(){
+        return id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+//    Setter
+
+    public void setId(int id){
         this.id = id;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(String username){
         this.username = username;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password){
         this.password = password;
     }
 
-    public void setNama(String nama) {
+    public void setNama(String nama){
         this.nama = nama;
     }
 
-    public void setRole(String role) {
+    public void setRole(String role){
         this.role = role;
     }
+
+    public abstract void tampilPeran();
+
 }
