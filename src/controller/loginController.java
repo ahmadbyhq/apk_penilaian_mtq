37bc62e3c2f9e2221ef803b7ea6376463fe86e4e
Ahmad Baihaqi <143946991+ahmadbyhq@users.noreply.github.com>
2025-05-28 12:38:56 +0700

    penyesuaian loginController

--- src/controller/loginController.java
+++ src/controller/loginController.java
@@ -7,6 +7,8 @@ package controller;
 import config.dbConnection;
 import java.sql.*;
 import model.User;
+import model.Panitia;
+import model.Juri;
 /**
  *
  * @author baiha
@@ -24,13 +26,21 @@ public class loginController {
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
-                return new User (
-                    rs.getInt("id_user"),
-                    rs.getString("username"),
-                    rs.getString("password"),
-                    rs.getString("nama"),
-                    rs.getString("role")
-                );
+            int id = rs.getInt("id_user");
+            String username = rs.getString("username");
+            String password = rs.getString("password");
+            String nama = rs.getString("nama");
+            String role = rs.getString("role");
+
+            switch (role.toLowerCase()) {
+                case "panitia":
+                    return new Panitia(id, username, password, nama);
+                case "juri":
+                    return new Juri(id, username, password, nama);
+                default:
+                    System.out.println("Role tidak dikenali: " + role);
+                    return null;
+                }
             }
 
         } catch (SQLException e) {
