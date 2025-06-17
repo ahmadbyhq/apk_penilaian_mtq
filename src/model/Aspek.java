15a6544073b0dcb15b8f675eecf0c84fb7435599
chxbim <christianbimo08@gmail.com>
2025-06-17 22:35:15 +0700

    class Aspek

---
+++ src/model/Aspek.java
@@ -0,0 +1,34 @@
+public class Aspek {
+    private int id_aspek;
+    private int id_lomba;
+    private String nama_aspek;
+    private double presentase;
+
+    public Aspek(int id_aspek, int id_lomba, String nama_aspek, double presentase) {
+        this.id_aspek = id_aspek;
+        this.id_lomba = id_lomba;
+        this.nama_aspek = nama_aspek;
+        this.presentase = presentase;
+    }
+
+    public int getid_aspek() {
+        return id_aspek;
+    }
+
+    public int getid_lomba() {
+        return id_lomba;
+    }
+
+    public String getnama_aspek() {
+        return nama_aspek;
+    }
+
+    public double getPresentase() {
+        return presentase;
+    }
+
+    @Override
+    public String toString() {
+        return nama_aspek + " (" + presentase + "%)";
+    }
+}
