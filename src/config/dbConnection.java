87a8b18ca14fb51e1bc696619d356d67f1f2d548
ahmadbyhq <baihaqiahmad641@gmail.com>
2025-06-18 23:59:44 +0700

    fix fitur ekspor pdf

--- src/config/dbConnection.java
+++ src/config/dbConnection.java
@@ -3,7 +3,7 @@ package config;
 import java.sql.*;
 
 public class dbConnection {
-    
+//    koneksi lokal
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/u725894752_app_mtq";
     private static final String USERNAME = "root";
@@ -17,4 +17,19 @@ public class dbConnection {
         }
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
+    
+    // koneksi db ke hostinger
+//    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+//    private static final String URL = "jdbc:mysql://auth-db1410.hstgr.io:3306/u725894752_app_mtq";
+//    private static final String USERNAME = "u725894752_appmtqpbo";
+//    private static final String PASSWORD = "Appmtqpbo{07};";
+//
+//    public static Connection getConnection() throws SQLException {
+//        try {
+//            Class.forName(JDBC_DRIVER);
+//        } catch (ClassNotFoundException e) {
+//            throw new SQLException("JDBC Driver tidak ditemukan: " + e.getMessage());
+//        }
+//        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+//    }
 }
