616a2ad379cf8901c04cce5fd47a3a19b5b69fc3
ahmadbyhq <baihaqiahmad641@gmail.com>
2025-06-19 14:39:12 +0700

    update tampilan di run project

--- src/main/main.java
+++ src/main/main.java
@@ -7,10 +7,23 @@ import model.*;
 import java.util.*;
 import ui.*;
 import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(()-> {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
             new login_fix().setVisible(true);
         });
     }
